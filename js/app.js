6ef48fd2291d9fa88ac75e6b964d59c7781abca1
sabbirahmedsr <sabbirahmedsr@gmail.com>
2025-08-23 08:31:24 +0600

    First sample index

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// This function will be called when the button is clicked
+function changeText() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = "The button was clicked!";
+}
